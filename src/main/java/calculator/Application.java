1562b7ae3f2f22bf70a7ea8a0e4babc8c080941d
Habin Kim <habin81111@naver.com>
2025-02-07 01:04:30 +0900

    feat: 커스텀 구분자 확인

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,9 +6,16 @@ public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String words;
+        char custom;
 
+        // 문자열 입력
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         words = sc.next();
-        System.out.println(words);
+
+        // 커스텀 구분자 확인
+        if(words.charAt(0)=='/' && words.charAt(1)=='/' && words.charAt(3)=='\\' && words.charAt(4)=='n') {
+            custom = words.charAt(2);
+        }
+
     }
 }
