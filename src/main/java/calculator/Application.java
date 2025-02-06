aa713cf7bbf0ee26ef5a39f8147a8de5a39ec5db
Habin Kim <habin81111@naver.com>
2025-02-06 22:51:23 +0900

    feat: 문자열 입력받기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        String words;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        words = sc.next();
+        System.out.println(words);
     }
 }
